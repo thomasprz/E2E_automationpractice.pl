85e5f2a82fc44b3e07a896d11ae7006ffcdd7a53
Thomas <przegralek.thomas@gmail.com>
2025-04-02 21:32:03 +0200

    Ajout des méthodes expectErrorAuthentification et expectErrorExistingEmailSignup

--- cypress/pages/authentication.page.js
+++ cypress/pages/authentication.page.js
@@ -1,3 +1,4 @@
+import data from '../fixtures/authentication.json'
 import BasePage from './base.page'
 
 class AuthenticationPage extends BasePage {
@@ -10,10 +11,13 @@ class AuthenticationPage extends BasePage {
         this.locatorCreateAccountButton = '#SubmitCreate'
         this.locatorSignInButton = '#SubmitLogin'
         this.locatorForgotPasswordLink = '.lost_password'
+        this.locatorErrorLoginMessage = '.alert-danger:nth-child(2)'
+        this.locatorErrorRegister = '.clearfix .alert-danger'
+
     }
 
-    expectAuthenticationPage(){
-        cy.url().should('include', 'my-account')
+    expectAuthenticationPage() {
+        cy.url().should('include', 'controller=authentication')
     }
 
     fillCreateAccountForm(email){
@@ -25,6 +29,14 @@ class AuthenticationPage extends BasePage {
         cy.get(this.locatorEmailLoginInput).type(email)
         cy.get(this.locatorPasswordInput).type(password)
         cy.get(this.locatorSignInButton).click()
+    }
+
+    expectErrorAuthentification(){
+        cy.get(this.locatorErrorLoginMessage).should('be.visible').and('contain.text',data.login_error)
+    }
+
+    expectErrorExistingEmailSignup(){
+        cy.get(this.locatorErrorRegister).should('be.visible').and('contain.text',data.email_exist_error)
     }
 
 }
