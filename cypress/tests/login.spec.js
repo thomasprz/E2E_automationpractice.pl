aa946c3ec199d7ee6dbb9a743e2ba13f054b5295
Thomas <przegralek.thomas@gmail.com>
2025-04-02 21:33:26 +0200

    Ajout de nouveaux tests liés au login

--- cypress/tests/login.spec.js
+++ cypress/tests/login.spec.js
@@ -3,8 +3,8 @@ import { header } from '../fixtures/components.fixture'
 import { generateRegisterData } from '../factory/register.factory'
 
 describe('Login', () => {
-  before(() => {
-    homepage.goTo('/')
+  beforeEach(() => {
+    homepage.goTo((Cypress.env('BASE_URL')))
     homepage.expectHomePage()
   })
   it('Inscription d\'un utilisateur', () => {
@@ -19,6 +19,54 @@ describe('Login', () => {
     createaccount.fillPersonalInformationForm(userData)
     //Assert
     myaccount.expectMyAccountPage() 
+    myaccount.verifyTitleVisible()
     myaccount.expectAccountCreated()
+    header.expectLoggedIn(userData.firstname,userData.lastname)
+  })
+
+  it('Connexion de l\'utilisateur avec un email et un mot de passe corrects', () => {
+    //Act
+    header.clickSignIn()
+    authentication.expectAuthenticationPage()
+    authentication.fillLoginForm(Cypress.env('EMAIL'), Cypress.env('PASSWORD'));
+    //Assert
+    myaccount.expectMyAccountPage()
+    myaccount.verifyTitleVisible()
+    header.expectLoggedIn(Cypress.env('FIRSTNAME'), Cypress.env('LASTNAME'));
+  })
+
+  it.only('Connexion de l\'utilisateur avec un email et un mot de passe incorrects', () => {
+    //Arrange
+    const userData = {
+      email: 'incorrect@email.com',
+      password: 'mauvais_mot_de_passe'
+    }
+    //Act
+    header.clickSignIn()
+    authentication.expectAuthenticationPage()
+    authentication.fillLoginForm(userData.email, userData.password);
+    //Assertion
+    authentication.expectErrorAuthentification()
+  })
+
+  it('Déconnexion de l\'utilisateur', () => {
+    //Act
+    header.clickSignIn()
+    authentication.expectAuthenticationPage()
+    authentication.fillLoginForm(Cypress.env('EMAIL'), Cypress.env('PASSWORD'));
+    myaccount.expectMyAccountPage()
+    myaccount.verifyTitleVisible()
+    header.expectLoggedIn(Cypress.env('FIRSTNAME'), Cypress.env('LASTNAME'));
+    header.clickSignOut()
+    //Assert
+    header.expectLoggedOff();
+  })
+
+  it('Inscription avec un email déjà existant', () => {
+    //Act
+    header.clickSignIn()
+    authentication.expectAuthenticationPage()
+    authentication.fillCreateAccountForm(Cypress.env('EMAIL'))
+    authentication.expectErrorExistingEmailSignup()
   })
 })
