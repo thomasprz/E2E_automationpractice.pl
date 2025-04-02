69fe4f5c6e15fbf212b40bb15a8ce801e77c7f85
Thomas <przegralek.thomas@gmail.com>
2025-04-02 21:30:30 +0200

    Mise à jour de la configuration Cypress avec de nouveaux paramètres

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://www.automationpractice.pl/',
     setupNodeEvents(on, config) {
+      config.env.BASE_URL = process.env.BASE_URL;
+      config.env.EMAIL = process.env.EMAIL;
+      config.env.PASSWORD = process.env.PASSWORD;
+      config.env.FIRSTNAME = process.env.FIRSTNAME;
+      config.env.LASTNAME = process.env.LASTNAME;
+      return config;
     },
     specPattern: 'cypress/tests/**/*.spec.js'
   },
