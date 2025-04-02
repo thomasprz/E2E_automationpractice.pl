ef07661f79d69d66d653696c349dee42cd906d7c
Thomas <przegralek.thomas@gmail.com>
2025-04-02 21:31:16 +0200

    Ajout de l'objet contact aux fixtures de pages

--- cypress/fixtures/pages.fixture.js
+++ cypress/fixtures/pages.fixture.js
@@ -2,15 +2,18 @@ import  AuthenticationPage from '../pages/authentication.page.js';
 import  MyAccountPage from '../pages/my-account.page.js';
 import  CreateAccountPage  from '../pages/create-account.page.js';
 import  HomePage  from '../pages/home.page.js'
+import ContactPage from '../pages/contact.page.js'
 
 const authentication = new AuthenticationPage();
 const homepage= new HomePage();
 const myaccount = new MyAccountPage();
 const createaccount = new CreateAccountPage();
+const contact = new ContactPage();
 
 export {
   authentication,
   homepage,
   myaccount,
   createaccount,
+  contact,
 };
