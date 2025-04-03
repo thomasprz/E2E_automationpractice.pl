e27167d344be41c19829476b52911d05f7ff70b6
Thomas <przegralek.thomas@gmail.com>
2025-04-03 22:38:21 +0200

    Mise à jour du fichier pages.fixture.js

--- cypress/fixtures/pages.fixture.js
+++ cypress/fixtures/pages.fixture.js
@@ -3,12 +3,16 @@ import  MyAccountPage from '../pages/my-account.page.js';
 import  CreateAccountPage  from '../pages/create-account.page.js';
 import  HomePage  from '../pages/home.page.js'
 import ContactPage from '../pages/contact.page.js'
+import ProductsPage from '../pages/products.page.js'
+import { ProductDetails } from '../pages/product-details.page.js';
 
 const authentication = new AuthenticationPage();
 const homepage= new HomePage();
 const myaccount = new MyAccountPage();
 const createaccount = new CreateAccountPage();
 const contact = new ContactPage();
+const products = new ProductsPage();
+const productdetail= new ProductDetails();
 
 export {
   authentication,
@@ -16,4 +20,6 @@ export {
   myaccount,
   createaccount,
   contact,
+  products,
+  productdetail,
 };
