476750d7ef3ab5f0237036cc69ab142f1da5fba9
Thomas <przegralek.thomas@gmail.com>
2025-04-04 23:06:19 +0200

    Ajout de nouveaux cas de tests

--- cypress/tests/products.spec.js
+++ cypress/tests/products.spec.js
@@ -1,4 +1,4 @@
-import { products, homepage, productdetail } from "../fixtures/pages.fixture";
+import { products, homepage, productdetail, cart, authentication} from "../fixtures/pages.fixture";
 import { header } from "../fixtures/components.fixture";
 
 
@@ -6,6 +6,9 @@ describe('Products', () => {
     beforeEach(() => {
         homepage.goTo((Cypress.env('BASE_URL')))
         homepage.expectHomePage()
+        header.clickSignIn()
+        authentication.expectAuthenticationPage()
+        authentication.fillLoginForm(Cypress.env('EMAIL'), Cypress.env('PASSWORD'));
     })
 
     it('Vérifier tous les produits et la page de détail du produit', () => {
@@ -45,5 +48,54 @@ describe('Products', () => {
         header.searchBar(unknownProduct.search)
         //Assert
         products.expectNoResultSearch(unknownProduct.search)
+    })
+
+    it.only('Ajouter un produit au panier et vérifier sa présence dans le panier', () => {
+        //Arrange
+        const productData = {
+            name : 'Printed Dress',
+            quantity : 1,
+            price : 51,
+            color:'Pink',
+            size:'M'
+        }
+        //Act
+        header.clickWomenMenu()
+        products.expectProductsPage()
+        products.clickProductTitle(productData)
+        productdetail.selectProductSize(productData.size)
+        productdetail.clickProductColor(productData.color)
+        productdetail.expectAvailableProduct()
+        productdetail.clickAddToCart()
+        productdetail.expectProductAddedToCart()
+        productdetail.expectProductDetailsPopUp(productData)
+        productdetail.clickProceedToCheckout()
+        cart.expectOneProductInCart(productData)
+    })
+
+    it('Modifier la quantité d\'un produit dans le panier et vérifier sa mise à jour', () => {
+        //Arrange
+        const productData = {
+            name : 'Printed Dress',
+            quantity : '4',
+            price : 51,
+            color:'Pink',
+            size:'M'
+        }
+        //Act
+        header.clickWomenMenu()
+        products.expectProductsPage()
+        products.clickProductTitle(productData)
+        productdetail.selectProductSize(productData.size)
+        productdetail.clickProductColor(productData.color)
+        productdetail.expectAvailableProduct()
+        productdetail.fillQuantityProduct(productData.quantity)
+        productdetail.clickAddToCart()
+        productdetail.expectProductAddedToCart()
+        productdetail.expectProductDetailsPopUp(productData)
+        productdetail.clickContinueShopping()
+        productdetail.clickProceedToCheckout()
+        cart.expectCartPage()
+        cart.expectOneProductInCart(productData)
     })
 })
